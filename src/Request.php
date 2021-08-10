3f17caf4fb7adc4a0af42f93222d8bb297668928
luwake <lubiao39699492@163.com>
2021-08-10 17:12:57 +0800

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -1,82 +1,145 @@
 <?php
 namespace Luwake;
 
-use Opis\Http\Request as HttpRequest;
-use Luwake\Traits\ObjectTrait;
-use Luwake\Traits\ExtendTrait;
-use Aura\Accept\AcceptFactory;
-use Opis\Http\ProxyHandler;
+use React\Http\Message\ServerRequest;
+use React\Http\Message\Response as ServerResponse;
 
-class Request extends HttpRequest implements \ArrayAccess
+/**
+ * 
+ * @author Administrator
+ * @property string $baseUrl
+ * @property array $body
+ * @property array $cookies
+ * @property boolean $fresh
+ * @property string $host
+ * @property string $hostname
+ * @property string $ip
+ * @property array $ips
+ * @property string $method
+ * @property string $originalUrl
+ * @property string $path
+ * @property string $protocol
+ * @property array $query
+ * @property boolean $secure
+ * @property array $signedCookies
+ * @property boolean $stale
+ * @property string $subdomains
+ * @property boolean $xhr
+ */
+class Request
 {
-    use ObjectTrait,ExtendTrait;
 
-    public $app;
+    /**
+     * Application
+     *
+     * @var Application
+     */
+    private $app;
 
-    public $accept;
+    /**
+     * PSR-7 Request
+     *
+     * @var ServerRequest
+     */
+    private $request;
 
-    public function __construct(Express $app)
+    /**
+     * PSR-7 Response
+     *
+     * @var ServerResponse
+     */
+    private $response;
+
+    public $route;
+
+    public $params = [];
+
+    public function __construct(Application $app)
     {
-        parent::__construct($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER, null);
         $this->app = $app;
-        $this->proxy = new ProxyHandler();
+        
+        $this->request = clone $app->request;
+        
+        $this->response = clone $app->response;
     }
 
-    private function accept()
+    public function accepts()
+    {}
+
+    public function acceptsCharsets()
+    {}
+
+    public function acceptsEncodings()
+    {}
+
+    public function acceptsLanguages()
+    {}
+
+    public function get()
+    {}
+
+    public function is()
+    {}
+
+    public function param()
+    {}
+
+    public function range()
+    {}
+
+    public function __get($name)
     {
-        if (! $this->accept) {
-            $this->accept = (new AcceptFactory($_SERVER))->newInstance();
+        switch ($name) {
+            case 'baseUrl':
+                return '/';
+            case 'body':
+                return $this->request->getParsedBody();
+            case 'cookies':
+                return $this->request->getCookieParams();
+            case 'fresh':
+                break;
+            case 'host':
+                break;
+            case 'hostname':
+                break;
+            case 'ip':
+                break;
+            case 'ips':
+                break;
+            case 'method':
+                return $this->request->getMethod();
+            case 'originalUrl':
+                break;
+            case 'path':
+                return $this->request->getUri()->getPath();
+            case 'protocol':
+                if ($this->request->getHeaderLine('HTTPS') == 'on') {
+                    return 'https';
+                }
+                if ($this->request->getHeaderLine('X_FORWARDED_PROTO') == 'https') {
+                    return 'https';
+                }
+                return 'http';
+            case 'query':
+                return $this->request->getQueryParams();
+            case 'secure':
+                if ($this->request->getHeaderLine('HTTPS') == 'on') {
+                    return true;
+                }
+                if ($this->request->getHeaderLine('X_FORWARDED_PROTO') == 'https') {
+                    return true;
+                }
+                return false;
+            case 'signedCookies':
+                break;
+            case 'stale':
+                break;
+            case 'subdomains':
+                break;
+            case 'xhr':
+                break;
         }
-        return $this->accept;
-    }
-
-    public function is($types)
-    {
-        return $this->accepts($types);
-    }
-
-    public function accepts($types)
-    {
-        return $this->accept()
-            ->negotiateMedia($types)
-            ->getValue();
-    }
-
-    public function acceptsCharsets($charset)
-    {
-        return $this->accept()
-            ->negotiateCharset($charset)
-            ->getValue();
-    }
-
-    public function acceptsEncodings($encoding)
-    {
-        return $this->accept()
-            ->negotiateEncoding($encoding)
-            ->getValue();
-    }
-
-    public function acceptsLanguages($lang)
-    {
-        return $this->accept()
-            ->negotiateLanguage($lang)
-            ->getValue();
-    }
-
-    public function param($name, $defaultValue = null)
-    {
-        if (($value = $this->get($name)) !== null) {
-            return $value;
-        }
-        if (($value = $this->post($name)) !== null) {
-            return $value;
-        }
-        if (($value = $this->file($name)) !== null) {
-            return $value;
-        }
-        if (($value = $this->getData($name)) !== null) {
-            return $value;
-        }
-        return $defaultValue;
+        ;
+        return null;
     }
 }
