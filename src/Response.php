ecb1a7a2fc85d9def8eede713c4eee98889b131b
luwake <lubiao39699492@163.com>
2021-08-10 17:13:14 +0800

    Update Response.php

--- src/Response.php
+++ src/Response.php
@@ -1,95 +1,247 @@
 <?php
 namespace Luwake;
 
-use Opis\Http\Response as HttpResponse;
-use Luwake\Traits\ObjectTrait;
-use Luwake\Traits\ExtendTrait;
+use React\Http\Message\ServerRequest;
+use React\Http\Message\Response as ServerResponse;
+use function RingCentral\Psr7\mimetype_from_extension;
 
-class Response extends HttpResponse implements \ArrayAccess
+class Response
 {
-    use ObjectTrait,ExtendTrait;
 
-    public $app;
+    /**
+     * Application
+     *
+     * @var Application
+     */
+    private $app;
+    
+    /**
+     * PSR-7 Request
+     *
+     * @var ServerRequest
+     */
+    private $request;
 
-    public function __construct(Express $app)
+    /**
+     * PSR-7 Response
+     *
+     * @var ServerResponse
+     */
+    private $response;
+
+    private $headersSent;
+
+    private $locals;
+
+    public function __construct(Application $app)
     {
-        parent::__construct($app->request());
         $this->app = $app;
+        
+        $this->request = clone $app->request;
+        
+        $this->response = clone $app->response;
     }
 
-    public function download($path, $filename = null, $fn = null)
+    public function append($field, $value)
+    {
+        $this->response = $this->response->withAddedHeader($field, $value);
+        
+        return $this;
+    }
+
+    public function attachment($filename)
     {}
 
-    public function end($data = null, $encoding = null)
+    public function cookie($name, $value, $options = [])
+    {
+        $this->response = $this->response->withAddedHeader('Cookie');
+        
+        return $this;
+    }
+
+    public function clearCookie($name, $options = [])
+    {
+        $this->response = $this->response->withHeader('Cookie', '-1');
+        
+        return $this;
+    }
+
+    public function download()
     {}
+
+    public function end($data, $encoding = null)
+    {
+        if ($data) {
+            $this->send($data);
+        }
+        
+        $this->set('Content-Length', $this->response->getBody()
+            ->getSize());
+        
+        return $this;
+    }
 
     public function format($object)
     {}
 
     public function get($field)
     {
-        return isset($this->headers[$field]) ? $this->headers[$field] : false;
+        return $this->response->getHeaderLine($field);
+    }
+
+    public function json($body = null)
+    {
+        return $this->type('json')->send($body);
+    }
+
+    public function jsonp($body = null)
+    {
+        $callback = isset($this->request->getQueryParams()['callback']) ? $this->request->getQueryParams()['callback'] : '';
+        
+        if ($callback) {
+            
+            $callback = preg_replace('/[^\[\]\w$.]/g', '', $callback);
+            
+            $body = json_encode($body);
+            $body = preg_replace('/\u2028/g', '\\u2028', $body);
+            $body = preg_replace('/\u2029/g', '\\u2029', $body);
+            
+            $body = '/**/ typeof ' + $callback + ' === \'function\' && ' + $callback + '(' + $body + ');';
+            
+            return $this->type('js')->send($body);
+        }
+        
+        return $this->json($body);
+    }
+
+    public function links($links)
+    {
+        $link = $this->get('Link');
+        
+        foreach ($links as $res => $link) {
+            $link[] = '<' + $link + '>; rel="' + $res + '"';
+        }
+        
+        $this->set('Link', implode(',', $link));
+        
+        return $this;
+    }
+
+    public function location($path)
+    {
+        if($path == 'back'){
+            $path = $this->request->getHeaderLine('Referrer') ? : '/';
+        }
+        return $this->set('Location', $path);
+    }
+
+    public function redirect($status, $path = null)
+    {
+        if (is_string($status)) {
+            $path = $status;
+            $status = 302;
+        }
+        $this->set('Location', $path)->status($status);
+        
+        return $this;
+    }
+
+    public function render($name, $options, $callback)
+    {
+        return $this->app->render($name, $options, $callback);
+    }
+
+    public function send($body)
+    {
+        switch (gettype($body)) {
+            case 'boolean':
+            case 'integer':
+            case 'double':
+            case 'string':
+                if (! $this->get('Content-Type')) {
+                    $this->type('html');
+                }
+                break;
+            case 'array':
+            case 'object':
+                if (! $this->get('Content-Type')) {
+                    $this->type('json');
+                }
+                $body = json_encode($body);
+                break;
+            case 'resource':
+            case 'resource (closed)':
+                if (! $this->get('Content-Type')) {
+                    $this->type('txt');
+                }
+                $body = (string) $body;
+                break;
+            case 'NULL':
+            case 'unknown type':
+                $body = null;
+        }
+        
+        if ($body) {
+            $this->response->getBody()->write($body);
+        }
+        
+        return $this;
+    }
+
+    public function sendFile($path, $options = [], $fn = null)
+    {}
+
+    public function sendStatus($statusCode)
+    {
+        $this->status($statusCode)->type('txt')->send($this->response->getReasonPhrase());
+        
+        return $this;
     }
 
     public function set($field, $value = null)
     {
         if (is_array($field)) {
-            return $this->headers($field);
+            foreach ($field as $key => $val) {
+                $this->set($key, $val);
+            }
+        } else {
+            $this->response = $this->response->withHeader($field, $value);
         }
-        return $this->header($field, $value);
-    }
-
-    public function json($body = null)
-    {
-        return $this->write(json_encode($body))->contentType('application/json');
-    }
-
-    public function jsonp($body = null)
-    {
-        $callback = $this->app->get('jsonp callback name');
-        return $this->write($callback . '(' . json_encode($body) . ')')->contentType('application/json');
-    }
-
-    public function links($links)
-    {}
-
-    public function location($path)
-    {
-        $this->set('Location', $path);
+        
         return $this;
     }
 
-    public function render($view, $locals = [], $handle = null)
+    public function status($code)
     {
-        return $this->app->render($view, $locals, $handle);
+        $this->response = $this->response->withStatus($code);
+        
+        return $this;
     }
 
-    public function write($body = null)
+    public function type($type)
     {
-        return $this->body($body);
+        return $this->set('Content-Type', mimetype_from_extension($type))->charset();
     }
-
-    public function sendFile($path, $options = null, $fn = null)
-    {}
-
-    public function sendStatus($statusCode)
+    
+    public function charset($encoding = null)
     {
-        return $this->status($statusCode);
+        $encoding = $encoding ? $encoding : 'utf-8';
+        
+        $type = $this->get('Content-Type');
+        if ($type) {
+            $this->set('Content-Type', $type . '; charset=' . $encoding);
+        }
+        
+        return $this;
     }
 
     public function vary($field)
-    {}
-
-    public function append($field, $value = null)
     {
-        return $this->set($field, $value);
+        return $this->set('Vary', $field);
     }
-
-    public function attachment($filename = null)
-    {}
-
-    public function clearCookie($name, $options = [])
+    
+    public function response()
     {
-        $this->deleteCookie($name, $options);
+        return $this->response;
     }
 }
