714af6a2dce3599b86371f0596fa6bfe78afa082
luwake <lubiao39699492@163.com>
2021-08-10 17:13:57 +0800

    Update Route.php

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -1,60 +1,87 @@
 <?php
 namespace Luwake\Router;
 
+use PetrGrishin\Pipe\Pipe;
 use Luwake\Request;
 use Luwake\Response;
+use Luwake\Router;
 
 class Route
 {
-
-    public $handle;
-
-    public $method;
-
+    /**
+     * @var Router
+     */
+    public $router;
+    
     public $path;
+    
+    private $stack = [];
 
-    public $keys = [];
-
-    public $regexp;
-
-    public $params = [];
-
-    public function __construct($path, $method, $fn)
+    public function __construct($path)
     {
-        $this->path = $path ? $path : '/';
-        $this->method = $method;
-        $this->handle = $fn;
-        $this->regexp = \PathToRegexp::convert($path, $this->keys);
+        $this->path = $path;
     }
 
-    public function match(Request $req)
+    public function path()
     {
-        $path = $req->path();
+        return $this->path;
+    }
+    
+    public function resolve($path = '')
+    {
+        return rtrim(str_replace('//', '/', $this->router->resolve($this->path) . '/' . trim($path, '/')), '/');
+    }
+
+    public function get($handler)
+    {
+        return $this->map('get', func_get_args());
+    }
+
+    public function post($handler)
+    {
+        return $this->map('post', func_get_args());
+    }
+
+    public function put($handler)
+    {
+        return $this->map('put', func_get_args());
+    }
+
+    public function delete($handler)
+    {
+        return $this->map('delete', func_get_args());
+    }
+
+    public function options($handler)
+    {
+        return $this->map('options', func_get_args());
+    }
+    
+    public function all($handler)
+    {
+        return $this->map('all', func_get_args());
+    }
+    
+    private function map($method, $handler)
+    {
+        $layer = new Layer('/', [], $handler);
         
-        $method = $req->method();
+        $layer->route = $this;
         
-        if ($this->method == 'ALL' || $this->method == $method) {
-            if ($path == null) {
-                return true;
-            }
-            if (strpos($path, $this->path) === 0) {
-                return true;
-            }
-            $match = \PathToRegexp::match($this->regexp, $path);
-            if ($match) {
-                $this->params = array_slice($match, 1);
-                return true;
-            }
-        }
-        return false;
+        $layer->method = $method;
+        
+        $this->stack[] = $layer;
+        
+        return $layer;
+    }
+
+    public function handle(Request $req, Response $res, $next)
+    {
+        return Pipe::create($req, $res)->through($this->stack)->then($next);
     }
 
     public function __invoke(Request $req, Response $res, $next)
     {
-        $fn = $this->handle;
-        if ($this->match($req)) {
-            return $fn($req, $res, $next);
-        }
-        return $next($req, $res);
+        return $this->handle($req, $res, $next);
     }
 }
